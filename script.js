5d812068ee8bf0dd1cfda35b137757f5ebcaf6f7
pphuwis <amphuwis@gmail.com>
2026-06-10 23:09:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-const overlay = document.getElementById("overlay");
-const main = document.getElementById("main");
-const music = document.getElementById("bgmusic");
-
-overlay.addEventListener("click", ()=>{
-
-    overlay.classList.add("hide");
-
-    setTimeout(()=>{
-        main.classList.add("show");
-    },300);
-
-    music.play();
-
-});
-
 document.getElementById("arrow").onclick = ()=>{
 
     document.querySelector(".profile").style.transform =
-    "scale(.85) translateY(-150px)";
+    "scale(.85)";
 
-    document.querySelector(".title").style.opacity = "0";
-    document.querySelector(".desc").style.opacity = "0";
+    document.getElementById("profile-section")
+    .style.top = "18%";
+
+    document.querySelector(".title")
+    .style.opacity = "0";
+
+    document.querySelector(".desc")
+    .style.opacity = "0";
 
     document.getElementById("socials")
     .classList.add("show");
-
 };
